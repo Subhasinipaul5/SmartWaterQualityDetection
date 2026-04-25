69d0c367af32f5fb81e5a0128306af185df2aa95
Subhasini Paul <paulsubhasini31@gmail.com>
2026-04-26 01:19:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ connectDB();
 // ── Middleware ──────────────────────────────────────────────────────────────
 app.use(cors({
   origin: [
-    process.env.FRONTEND_URL || 'https://smart-water-detection.onrender.com/api',
+    process.env.FRONTEND_URL || 'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://localhost:5500',
+    'http://127.0.0.1:5500',
+    'https://smartwaterqualitydetection.netlify.app'
   ],
   credentials: true,
 }));
